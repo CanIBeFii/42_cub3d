ec1f1806d69450d7d148c627da3966599b7ee5c0
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-12 18:09:14 +0100

    updated close_window function given arguments

--- sources/handle_keys.c
+++ sources/handle_keys.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:06:34 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/11 16:08:20 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:37:47 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	keys(int key_code, t_game *mlx)
 {
 	(void)mlx;
 	if (key_code == 65307)
-		close_window();
+		close_window(mlx);
 	// else if (key_code == 119 || key_code == 97 || key_code == 115
 	// 	|| key_code == 100)
 	// 	player_movements(key_code, mlx);
