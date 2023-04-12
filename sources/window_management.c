8c0dc7933b82af4f83dc69fa2f51a64480b99f9f
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-12 18:10:37 +0100

    updated close_window function prototype to be able to receive the mlx variable

--- sources/window_management.c
+++ sources/window_management.c
@@ -6,14 +6,15 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:37:54 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/11 15:51:05 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:37:17 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	close_window(void)
+int	close_window(t_game *mlx)
 {
+	free_double_array(mlx->map);
 	exit (0);
 	return (0);
 }
