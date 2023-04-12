2aaae75e4b40d38076e5f7fa285abb2bae428c10
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-12 18:12:37 +0100

    map checker now checks if the first 6 line, beside newlines, are valid element types and if they're not repeated

--- sources/map_check.c
+++ sources/map_check.c
@@ -6,11 +6,30 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:36:59 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/11 18:40:57 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/04/12 18:04:27 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	check_map_elements(t_game *mlx, char *line)
+{
+	if (ft_strncmp(line, "NO ", 3) == 0 && mlx->map_info.north == 0)
+		mlx->map_info.north = 1;
+	else if (ft_strncmp(line, "SO ", 3) == 0 && mlx->map_info.south == 0)
+		mlx->map_info.south = 1;
+	else if (ft_strncmp(line, "WE ", 3) == 0 && mlx->map_info.west == 0)
+		mlx->map_info.west = 1;
+	else if (ft_strncmp(line, "EA ", 3) == 0 && mlx->map_info.east == 0)
+		mlx->map_info.east = 1;
+	else if (ft_strncmp(line, "F ", 2) == 0 && mlx->map_info.floor == 0)
+		mlx->map_info.floor = 1;
+	else if (ft_strncmp(line, "C ", 2) == 0 && mlx->map_info.ceiling == 0)
+		mlx->map_info.ceiling = 1;
+	else
+		return (0);
+	return (1);
+}
 
 int	number_lines(int fd)
 {
@@ -44,7 +63,7 @@ char	**get_map(int fd, char *path)
 	int		nb_lines;
 
 	nb_lines = number_lines(fd);
-	map = malloc(sizeof(char *) * (nb_lines + 1));
+	map = malloc(sizeof(char *) * (nb_lines));
 	if (map == NULL)
 		return (NULL);
 	fd = open(path, O_RDONLY);
@@ -58,6 +77,9 @@ char	**get_map(int fd, char *path)
 		i++;
 		free(line);
 		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		map[i] = malloc(sizeof(char) * (ft_strlen(line) + 1));
 		map[i] = ft_strdup(line);
 	}
 	map[i] = NULL;
@@ -66,8 +88,26 @@ char	**get_map(int fd, char *path)
 
 int	map_check(int fd, char *path, t_game *mlx)
 {
+	int	lines;
+	int	i;
+
 	mlx->map = get_map(fd, path);
 	if (mlx->map == NULL)
 		return (0);
+	i = 0;
+	lines = 0;
+	while (mlx->map[i] != NULL && lines < 6)
+	{
+		// printf("line: %d %s\n", lines, mlx->map[i]);
+		if (mlx->map[i][0] == '\n')
+			i++;
+		else
+		{
+			if (check_map_elements(mlx, mlx->map[i]) == 0)
+				return (0);
+			i++;
+			lines++;
+		}
+	}
 	return (1);
 }
