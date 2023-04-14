1aa66604884db466342038eab53e58ded3ee1c84
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-14 23:01:18 +0100

    added draw_player, draw_line and draw_map functions

--- sources/movement.c
+++ sources/movement.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:34:05 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/14 17:50:19 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/04/14 21:26:15 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,8 @@ void	move_dot(int key_code, t_game *mlx)
 		mlx->player.pdy = sin(mlx->player.pa) * 5;
 	}
 	mlx_clear_window(mlx->mlx_ptr, mlx->window);
-	draw_square(mlx, mlx->player.pos_x, mlx->player.pos_y, 0x00FF0000);
+	draw_player(mlx, mlx->player.pos_x, mlx->player.pos_y, 0x00FF0000);
+	draw_line(mlx, mlx->player.pos_x + mlx->player.pdx * 10,
+		mlx->player.pos_y + mlx->player.pdy * 10);
+	draw_map(mlx);
 }
