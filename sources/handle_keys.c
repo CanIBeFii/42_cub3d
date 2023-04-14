1b32c0f8202c8f0764ab0c982587bde3bfa621d5
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-14 17:54:19 +0100

    function now handles the WASD keys

--- sources/handle_keys.c
+++ sources/handle_keys.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:06:34 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/12 16:37:47 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/04/14 15:05:11 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 int	keys(int key_code, t_game *mlx)
 {
-	(void)mlx;
 	if (key_code == 65307)
 		close_window(mlx);
-	// else if (key_code == 119 || key_code == 97 || key_code == 115
-	// 	|| key_code == 100)
-	// 	player_movements(key_code, mlx);
+	else if (key_code == 119 || key_code == 97 || key_code == 115
+		|| key_code == 100)
+		move_dot(key_code, mlx);
 	return (0);
 }
