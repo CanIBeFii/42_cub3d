17f8a9d31dc0af5bde828b349cba309bb22d3f68
micas <miguelbernardinosousa2002@gmail.com>
2023-05-01 17:09:39 +0100

    add t_data struct and update/add funcitons prototypes

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:32:28 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/14 19:01:30 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:57:18 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,14 @@ typedef struct s_vector
 	int	y;
 }t_vector;
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
 typedef struct s_game
 {
 	void		*mlx_ptr;
@@ -59,12 +67,13 @@ typedef struct s_game
 	t_vector	window_size;
 	t_map		map_info;
 	t_player	player;
+	t_data		img;
 	char		**map;
 }t_game;
 
 //MAIN.C
 void	cub3d(int fd, char *path);
-void	game_init(t_game *mlx);
+void	game_init(t_game mlx);
 
 //WINDOW_MANAGEMENT.C
 int		close_window(t_game *mlx);
@@ -90,8 +99,10 @@ void	free_double_array(char **double_array);
 void	move_dot(int key_code, t_game *mlx);
 
 //DRAW.C
+void	my_img_clear(t_game *mlx);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 void	draw_square(t_game *mlx, int x, int y, int color);
-void	draw_line(t_game *mlx, int x2, int y2);
+void	draw_line(t_game *mlx, float x2, float y2);
 void	draw_map(t_game *mlx);
 void	draw_player(t_game *mlx, int x, int y, int color);
 
