4b6350b4740e4df67ad12a3b303dbaa4469eb6b6
micas <miguelbernardinosousa2002@gmail.com>
2023-05-01 17:06:24 +0100

    drawing now uses my_put_image_to_window

--- sources/movement.c
+++ sources/movement.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:34:05 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/17 18:12:05 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:57:35 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,11 @@ void	move_dot(int key_code, t_game *mlx)
 		mlx->player.pdx = cos(mlx->player.pa) * 5;
 		mlx->player.pdy = sin(mlx->player.pa) * 5;
 	}
-	mlx_clear_window(mlx->mlx_ptr, mlx->window);
+	my_img_clear(mlx);
 	draw_map(mlx);
 	draw_player(mlx, mlx->player.pos_x, mlx->player.pos_y, 0x00FF0000);
 	draw_line(mlx, mlx->player.pos_x + mlx->player.pdx * 10,
 		mlx->player.pos_y + mlx->player.pdy * 10);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->window, mlx->img.img, 0, 0);
+
 }
