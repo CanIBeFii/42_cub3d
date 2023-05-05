b9c004329a61778e3799cb9e6e6496d5def770d5
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-05-05 15:44:39 +0100

    update to my_img_clear function to clean the window with dynamic size

--- sources/draw.c
+++ sources/draw.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 18:38:10 by mibernar          #+#    #+#             */
-/*   Updated: 2023/05/02 17:39:00 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/05/05 15:21:26 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	my_img_clear(t_game *mlx)
 {
-	size_t	j;
-	size_t	i;
+	int	j;
+	int	i;
 
 	j = 0;
-	while (j < 800)
+	while (j < (mlx->map_info.map_y * 64))
 	{
 		i = 0;
-		while (i < 800)
+		while (i < (mlx->map_info.map_x * 64))
 		{
 			my_mlx_pixel_put(&mlx->img, i, j, 0x000000);
 			i++;
@@ -35,13 +35,13 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 void	draw_rays(t_player p, t_rays r, t_game mlx)
 {
 	float	a_tan;
-	
+
 	r.ra = p.pa;
 	r.r = 0;
 	while (r.r < 1)
