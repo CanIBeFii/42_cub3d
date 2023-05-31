10182e118d226ef83669819ec27b117b0b5eb50d
CanIBeFii <filipe250502@gmail.com>
2023-05-31 18:57:23 +0100

    feat: assign_path_to_info return if successful

--- sources/parser.c
+++ sources/parser.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 18:21:35 by canibefii         #+#    #+#             */
-/*   Updated: 2023/05/29 17:47:56 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/05/31 18:51:58 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_map	*parser(char *path)
 		return (NULL);
 }
 
-t_map_info	get_map_info(int fd)
+t_map_info	*get_map_info(int fd)
 {
 	t_map_info	info;
 	char		*line;
@@ -32,24 +32,39 @@ t_map_info	get_map_info(int fd)
 	{
 		if (is_possible_line(line) == 1)
 		{
-			assign_path_to_info(info, line);
+			if (assign_path_to_info(info, line) == 0)
+				return (NULL);
 		}
 		free(line);
 		line = get_next_line(fd);
 	}
+	return (&info);
 }
 
-void	assign_path_to_info(t_map_info info, char *line)
+int	assign_path_to_info(t_map_info info, char *line)
 {
 	int	iter;
 
 	iter = 1;
 	if (line[iter] != ' ')
 		iter += 1;
-	while (line[iter] == ' ')
+	while (ft_isspace(line[iter]) == 1)
 		iter += 1;
-	if (ft_strncmp("NO", line, 2) == 0)
-		
+	if (ft_strncmp("NO", line, 2) == 0 && info.north_texture == NULL)
+		info.north_texture = ft_strdup(line + iter);
+	else if (ft_strcmp("SO", line, 2) == 0 && info.south_texture == NULL)
+		info.south_texture = ft_strdup(line + iter);
+	else if (ft_strncmo("WE", line, 2) == 0 && info.west_texture == NULL)
+		info.west_texture = ft_strdup(line + iter);
+	else if (ft_strncmp("EA", line, 2) == 0 && info.east_texture == NULL)
+		info.east_texture = ft_strdup(line + iter);
+	else if (ft_strncmp("F", line, 1) == 0 && info.ground_color == 0)
+		info.ground_color = str_to_color(line + iter);
+	else if (ft_strncmp("C", line, 1) == 0 && info.sky_color == 0)
+		info.sky_color = str_to_color(line + iter);
+	else
+		return (0);
+	return (1);
 }
 
 int	is_possible_line(char *line)
