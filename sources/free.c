6c6eaf0a051ea2354b3e0caa4976f6c390e18e6c
CanIBeFii <filipe250502@gmail.com>
2023-06-01 16:36:55 +0100

    feat: add free functions to map and map info

--- sources/free.c
+++ sources/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 18:41:52 by mibernar          #+#    #+#             */
-/*   Updated: 2023/04/11 18:42:47 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/06/01 15:44:42 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,24 @@ void	free_double_array(char **double_array)
 		i++;
 	}
 	free(double_array);
+}
+
+void	free_map_info(t_map_info *info)
+{
+	if (info->north_texture != NULL)
+		free(info->north_texture);
+	if (info->south_texture != NULL)
+		free(info->south_texture);
+	if (info->west_texture != NULL)
+		free(info->west_texture);
+	if (info->east_texture != NULL)
+		free(info->east_texture);
+	free(info);
+}
+
+void	free_map(t_map *map)
+{
+	free_map_info(map->info);
+	free_double_array(map->map);
+	free(map);
 }
