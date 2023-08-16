b3ee30f6835214b232c0266cbf7c40375fa712c4
CanIBeFii <filipe250502@gmail.com>
2023-08-16 15:34:06 +0100

    add map validator functions

--- sources/map_validator.c
+++ sources/map_validator.c
@@ -6,18 +6,11 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 15:50:55 by fialexan          #+#    #+#             */
-/*   Updated: 2023/08/10 16:52:24 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/08/11 15:50:51 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-int	check_surroundings(t_map *map, int x, int y)
-{
-	if (x == 0 || x == map->x || y == 0 || y == map->y)
-		return (0);
-	return (1);
-}
 
 int	validate_map(t_map *map)
 {
@@ -38,4 +31,16 @@ int	validate_map(t_map *map)
 		y++;
 	}
 	return (1);
+}
+
+int	check_surroundings(t_map *map, int x, int y)
+{
+	if (x == 0 || x == map->x || y == 0 || y == map->y)
+		return (is_inside_map_char(map->map[y][x]) == 0);
+	return (1);
+}
+
+int	is_inside_map_char(char c)
+{
+	return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
 }
