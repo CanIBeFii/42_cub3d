2f79318131ee62a82742a71b0566b190803ebbfe
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-08-29 17:04:52 +0100

    working on drawing the walls, needs updates

---
+++ sources/draw.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 15:14:53 by mibernar          #+#    #+#             */
+/*   Updated: 2023/08/29 16:57:09 by mibernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	get_color(int tex_x, int tex_y, t_data *curr)
+{
+	int		bpp;
+	char	*data;
+	int		color;
+	int		pixel_index;
+	int		i;
+
+	data = mlx_get_data_addr(curr->img, &curr->bits_per_pixel,
+			&curr->line_length, &curr->endian);
+	bpp = curr->bits_per_pixel / 8;
+	color = 0;
+	pixel_index = tex_y * 64 + tex_x;
+	i = -1;
+	while (++i < bpp)
+		color |= data[(pixel_index * bpp) + i] << (8 * i);
+	return (color);
+}
+
+void	draw_walls(t_ray *ray, t_data *texture, double wall_height)
+{
+	double	step;
+	double	texture_pos;
+	int		color;
+	int		y;
+
+	step = texture->img_size.y / wall_height;
+	texture_pos = (ray->wall_start - SCREEN_H / 2 + wall_height / 2) * step;
+	y = ray->wall_start;
+	while (y < ray->wall_end)
+	{
+		color = get_color(, texture_pos, texture);
+		texture_pos += step;
+	}
+}
