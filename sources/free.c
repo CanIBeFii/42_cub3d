2c2c293b6814378d7be5a2bfb9448f825d8a9d6f
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-09-01 17:38:45 +0100

    fixed some leaks

--- sources/free.c
+++ sources/free.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 16:05:38 by fialexan          #+#    #+#             */
-/*   Updated: 2023/08/23 16:09:25 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/09/01 12:40:47 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ void	free_game(t_game *mlx)
 	if (mlx->map.map != NULL)
 		free_double_array(mlx->map.map);
 	if (mlx->info.no)
-		free(mlx->info.no);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->info.no_texture.img);
 	if (mlx->info.so)
-		free(mlx->info.so);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->info.so_texture.img);
 	if (mlx->info.ea)
-		free(mlx->info.ea);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->info.ea_texture.img);
 	if (mlx->info.we)
-		free(mlx->info.we);
-	// mlx_destroy_window(mlx->mlx_ptr, mlx->window);
-	// mlx_destroy_display(mlx->mlx_ptr);
-	// free (mlx->mlx_ptr);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->info.we_texture.img);
+	mlx_destroy_window(mlx->mlx_ptr, mlx->window);
+	mlx_destroy_display(mlx->mlx_ptr);
+	free (mlx->mlx_ptr);
 }
