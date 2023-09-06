5c9dd7f6666f433ff3626e5e9ed59ce47eaa2a5b
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-09-06 12:41:55 +0100

    fix segfault when given invalid number of arguments

--- sources/file_checker.c
+++ sources/file_checker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   file_checker.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 14:21:58 by fialexan          #+#    #+#             */
-/*   Updated: 2023/08/21 13:40:09 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/09/06 12:41:40 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,17 @@ void	check_file(int argc, char **argv, t_game *mlx)
 	int		x;
 
 	if (argc != 2)
-		print_error("Error: invalid number of arguments", 1);
+		exit(print_error("Error: invalid number of arguments", 1));
 	path = argv[1];
 	x = ft_strlen(path) - 1;
 	if (path[x] != 'b' || path[x - 1] != 'u' || path[x - 2] != 'c'
 		|| path[x - 3] != '.')
 	{
-		perror("Error: invalid file type");
-		exit (1);
+		exit (print_error("Error: invalid file type", 1));
 	}
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-	{
-		perror("Error: can't open file");
-		exit (1);
-	}
+		exit (print_error("Error: can't open file", 1));
 	mlx->path = path;
 	close(fd);
 }
